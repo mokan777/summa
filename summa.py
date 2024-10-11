124717b818bb4e20dc5799bfad4565661c22bb65
mokan777 <rota.podem@tutamail.com>
2024-10-11 16:50:03 +0300

    summa

--- summa.py
+++ summa.py
@@ -1,17 +1,11 @@
-input_data = open('input.txt', 'r') # открываем для чтения (литера 'r') файл и кладем его в переменую
-data = input_data.read() # читаем другую переменую содержимое всего файла
+n = int(input())
 
-for i in range(1 , 10^4):
-print(i, end="; ")
-
-output_data = open('output.txt', 'r')
-                   
-
-output_data = open('output.txt', 'w') # открываем для записи (литера 'w') файл и кладем его в переменую
-# записываем результат сложения и не забываем преобразовывать его в строку (иначе 
-output_data.write(c)
-# output_data.write(str(int(data[0]) + int(data[1])))
-# ВАЖНО!!!
-# не забываем закрывать открытые ранее файлы
-input_data.close()
-output_data.close()
+sum = 0
+if n>0:
+    for i in range(1 , n + 1):
+        sum = sum + i 
+else:
+    for i in range(n , 2):
+        sum = sum + i 
+b = str(sum)
+print(b)
